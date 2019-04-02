b34ff4ff4e021e8e3bb2cdbfe04101dca32971c4
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-04-02 20:49:01 -0300

    "Add basis script for installing on new machine"

---
+++ install_script.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ruby`ruby -e 'puts RUBY_VERSION[/\d+\.\d+/]'`-dev # Taken from https://stackoverflow.com/questions/20559255/error-while-installing-json-gem-mkmf-rb-cant-find-header-files-for-ruby
+
+sudo gem install bundler
+
+
